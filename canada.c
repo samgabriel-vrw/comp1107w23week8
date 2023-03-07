0145af582847e450f472536b7041cf77c0b76849
samgabriel-vrw <81274739+samgabriel-vrw@users.noreply.github.com>
2023-03-07 22:42:57 +0000

    Code by the end of Week 8 Lesson

--- canada.c
+++ canada.c
@@ -3,8 +3,38 @@
 
 const char *PROVINCES[] = {"Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan"};
 
-void print_provinces()
+void void print_provinces()
 {
     //TODO:: Write a for loop that iterates over the PROVINCES array and prints out how many letters is contained in each
 
+    
+    province_size = sizeof(PROVINCES)/sizeof(PROVINCES);
+
+    for (int i = 0; i < province_size, i++){
+
+    
+
+    }
+    
+    
+
+
+
+
+
+
+    string str = get_string("what string do you want to know the ascii alues of the letters of?");
+
+    int strSize = strlen(str);
+
+    int i = 0;
+
+    while (i < strSize)
+    {
+        int asciiVal = str[i]
+        printf("The char at position %d" is %c and the ASCII value is %d",i, str[i], str[i]);
+        i++;
+
+    }
+    for()
 }
